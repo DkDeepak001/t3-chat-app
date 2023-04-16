d140cd729e15c01e6171e8e455fb17500f4f87eb
DkDeepak001 <Deepakeswar001@gmail.com>
2023-04-16 20:20:12 +0530

    added googleAuth

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { signIn, useSession } from "next-auth/react";
+
+export default function home() {
+  const { data: session } = useSession();
+
+  const handleGooleLogin = async () => {
+    try {
+      await signIn("google");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      {!session ? <h2>login</h2> : <h2>sigin in as {session?.user.name}</h2>}
+      <h2></h2>
+      <button onClick={handleGooleLogin}>G sigin</button>
+    </div>
+  );
+}
