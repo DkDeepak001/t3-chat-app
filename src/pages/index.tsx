42ca4adce9133df2a279a9f389f6e05b6e9f4ae6
DkDeepak001 <Deepakeswar001@gmail.com>
2023-04-17 22:05:50 +0530

    feat: login ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,14 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export default function home() {
   const { data: session } = useSession();
 
-  const handleGooleLogin = async () => {
-    try {
-      await signIn("google");
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
-    <div>
-      {!session ? <h2>login</h2> : <h2>sigin in as {session?.user.name}</h2>}
-      <h2></h2>
-      <button onClick={!session ? handleGooleLogin : (): void => signOut()}>
-        {!session ? `sigin` : `sigin out`}
+    <div className="flex h-screen w-screen items-center justify-center bg-slate-400">
+      <button
+        onClick={(): void => signIn("google")}
+        type="button"
+        className="mb-2 mr-2 rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
+        Sigin with Google
       </button>
     </div>
   );
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,9 +6,11 @@ import {
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "y/env.mjs";
 import { prisma } from "y/server/db";
+import { redirect } from "next/dist/server/api-utils";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -48,14 +50,35 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    // DiscordProvider({
+    //   clientId: env.DISCORD_CLIENT_ID,
+    //   clientSecret: env.DISCORD_CLIENT_SECRET,
+    // }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+
+    // Credentials({
+    //   name: "credentials",
+    //   credentials: {
+    //     email: { label: "email", type: "text" },
+    //     password: { label: "password", type: "password" },
+    //   },
+    //   authorize(credentials, req) {
+    //     const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+    //     if (user) {
+    //       // Any object returned will be saved in `user` property of the JWT
+    //       return { id: "1", name: "J Smith", email: "jsmith@example.com" };
+    //     } else {
+    //       // If you return null then an error will be displayed advising the user to check their details.
+    //       return { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+    //       // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+    //     }
+    //   },
+    // }),
   ],
 };
 
