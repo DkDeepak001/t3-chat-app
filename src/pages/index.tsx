a06503409e7b7c6fbb0033cbe4707056370a9447
DkDeepak001 <Deepakeswar001@gmail.com>
2023-04-18 20:59:28 +0530

    feat: added ui to login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,55 @@
+import { error } from "console";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { TailSpin } from "react-loader-spinner";
 
 export default function home() {
   const { data: session } = useSession();
+  console.log(session);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleSigin = async () => {
+    try {
+      setIsLoading(true);
+      await signIn("google");
+    } catch (error) {
+      toast.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-slate-400">
+    <div className="flex h-screen w-screen items-center justify-center bg-white">
       <button
-        onClick={(): void => signIn("google")}
+        onClick={() => handleSigin()}
         type="button"
-        className="mb-2 mr-2 rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+        className=" mb-2 mr-2 rounded-lg bg-slate-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
       >
-        Sigin with Google
+        <div className="flex flex-row items-center justify-center gap-2">
+          {isLoading ? (
+            <TailSpin
+              height="25"
+              width="25"
+              color="black"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          ) : (
+            <Image
+              src={"/google.svg"}
+              alt="google svg"
+              width={25}
+              height={25}
+            />
+          )}
+          <h3 className="font-semibold text-black">Sigin with Google</h3>
+        </div>
       </button>
     </div>
   );
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -39,6 +39,7 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
@@ -47,38 +48,15 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
     }),
+    redirect() {
+      return "/dashboard";
+    },
   },
-  adapter: PrismaAdapter(prisma),
   providers: [
-    // DiscordProvider({
-    //   clientId: env.DISCORD_CLIENT_ID,
-    //   clientSecret: env.DISCORD_CLIENT_SECRET,
-    // }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-
-    // Credentials({
-    //   name: "credentials",
-    //   credentials: {
-    //     email: { label: "email", type: "text" },
-    //     password: { label: "password", type: "password" },
-    //   },
-    //   authorize(credentials, req) {
-    //     const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-    //     if (user) {
-    //       // Any object returned will be saved in `user` property of the JWT
-    //       return { id: "1", name: "J Smith", email: "jsmith@example.com" };
-    //     } else {
-    //       // If you return null then an error will be displayed advising the user to check their details.
-    //       return { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-    //       // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-    //     }
-    //   },
-    // }),
   ],
 };
 
